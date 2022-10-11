7f083233e995dccb125d4ac695d98ad0bebf4e9b
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-11 09:42:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 input.onButtonPressed(Button.AB, function () {
-	
+    for (let index = 0; index < 100; index++) {
+        sum += 1
+        sum = sum + num
+    }
+    basic.showNumber(sum)
 })
+let sum = 0
 let num = 0
+num = 0
+sum = 0
